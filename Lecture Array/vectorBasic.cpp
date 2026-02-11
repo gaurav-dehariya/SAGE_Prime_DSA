8342e1ce5f6612f628fd0f181a5c71324706588e
Gaurav Dehariya <gauravdehariya55@gmail.com>
2026-02-11 21:49:16 +0530

    Initial

---
+++ Lecture Array/vectorBasic.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    a.push_back(5);
+
+    cout<<a.size()<<endl;
+    cout<<a.capacity()<<endl;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << "  ";
+    }
+
+    cout << endl;
+    cout << a.front() << endl;
+    cout << a.back() << endl;
+
+    reverse(a.begin(),a.end());
+    sort(a.begin(), a.end());
+
+    // Insert Function
+    a.insert(a.end()-3,9);
+     for (int i = 0; i < a.size(); i++){
+        cout << a[i] << "  ";
+    }
+
+    cout<<endl;
+    int n=5;
+    vector <int> arr(n,1);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<"  ";
+    }
+    return 0;
+}
