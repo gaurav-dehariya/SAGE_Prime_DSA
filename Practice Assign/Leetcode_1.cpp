6b48b7a34e1c71e19e7d9b2e343a3c248ab345be
Gaurav Dehariya <gauravdehariya55@gmail.com>
2026-02-12 22:10:39 +0530

    Initial

---
+++ Practice Assign/Leetcode_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int main(){
+    vector<int> nums= {2,7,11,15};
+    int target=18;
+
+    for(int i=0; i<=nums.size(); i++){
+        for(int j=i+1; j<=nums.size(); j++){
+            if(nums[i]+nums[j]==target){
+                 cout << "Indices: " << i << " and " << j << endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"Not found."<<endl;
+    return 0;
+}
